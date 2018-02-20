f64daa3712e4e128d29b201ea0f59c16e800543a
devincody <devincody@gmail.com>
2018-02-19 18:20:59 -0800

    added optimization over focal lengths and adding functions to gen directories

--- process_grasp.py
+++ process_grasp.py
@@ -1,11 +1,12 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
+import pandas as pd
 
 
 def main():
 	PA = "F:/Devin/Grasp/LWASandbox/40mLWA/Job_26/"
 	freq, s11 = process_par(PA + "Sparameters.par")
-	dmax = process_cut(PA + "FieldData.cut", freq)
+	dmax, cut = process_cut(PA + "FieldData.cut", freq)
 	plot_pair_efficiencies(freq, s11, dmax, "Efficiencies.png", 16)
 
 
@@ -27,30 +28,50 @@ def process_cut(f_name, freq):
 	f = open(f_name)
 	line = f.readline()
 	dmax = []
+	i = -1
+	cut = pd.DataFrame()
 	while ("Field data" in line):
 		line = f.readline()
 		line = line.split()
 		line = [float(x) for x in line]
 		# print ("Headder = ", line)
+		phi = line[3]
+		if (phi == 0):
+			i += 1
+		frequency = freq[i]
+		series_name_co = "f%d:p%d" %(frequency, phi)
+		series_name_cx = "f%d:p%d" %(frequency, phi)
+
+		angles = []
+		dbi_co = []
+		dbi_cx = []
+
 		for ii in range(int(line[2])):
 			angle=line[0] + ii*line[1]
+			angles.append(angle)
 			fields = [float(x) for x in f.readline().split()]
 			co = 10*np.log10(fields[0]**2 + fields[1]**2)
 			cx = 10*np.log10(fields[2]**2 + fields[3]**2)
+
+			dbi_co.append(co)
+			dbi_cx.append(cx)
 			if ii == 100:
 				dmax.append(np.max([co, cx]))
+
+		cut[series_name_co] = dbi_co
+		cut[series_name_cx] = dbi_cx
 		line = f.readline() #should be Field data... if more data
 
 	dmax_f = np.zeros(len(freq))
 	for i in range(len(freq)):
 		dmax_f[i] = np.max(dmax[i*3:(i+1)*3])
 
-	return dmax_f
+	return dmax_f, cut
 
 def calc_mismatch(s11): 
 	#s11 in dB
 	#returns %
-	return (1-10**(s11/10))*100
+	return (1-10**(s11/10))
 
 def calc_app_eff(freq, dmax): 
 	#freq in MHz, dmax in dB
@@ -60,16 +81,16 @@ def calc_app_eff(freq, dmax):
 	wave_len = c/(freq*1E6)
 	aphy = np.pi*r**2
 	dmax_lin = 10**(dmax/10)
-	return dmax_lin*wave_len**2 /(4*np.pi*aphy)*100
+	return dmax_lin*wave_len**2 /(4*np.pi*aphy)
 
 
 def plot_pair_efficiencies(freq, s11, dmax, title, z):
 	plt.figure()
 	plt.plot(freq,calc_mismatch(s11), 'b', label= "Mismatch Efficiency")
-	plt.title("Mismatch and Aperture Efficiencies z = %3.1f" % z)
+	plt.title("Mismatch and Aperture Efficiencies z = %4.2f" % z)
 	plt.xlabel("Frequency [MHz]")
-	plt.ylabel("Efficiency [%]")
-	plt.ylim([0, 90])
+	plt.ylabel("Efficiency")
+	plt.ylim([0, 1])
 
 	plt.plot(freq,calc_app_eff(freq, dmax), 'r', label = "Aperture Efficiency")
 	# plt.set_ylabel("Aperture Efficiency [%]")
@@ -79,7 +100,36 @@ def plot_pair_efficiencies(freq, s11, dmax, title, z):
 	# plt.legend(lns, labs)
 
 	plt.legend()
-	plt.savefig("../plots/" + title)
+	plt.savefig("../plots/Efficiency/" + title)
+
+def plot_SEFD(freq, dmax, title, z):
+	plt.figure()
+	plt.plot(freq,SEFD(freq, dmax), 'b', label= "SEFD")
+	plt.title("SEFD at z = %4.2f" % z)
+	plt.xlabel("Frequency [MHz]")
+	plt.ylabel("SEFD")
+	plt.ylim([0, 5E6])
+
+	plt.legend()
+	plt.savefig("../plots/SEFD/" + title)
+
+
+def plot_cut(freq, cut):
+	#freq is which frequency to use
+	#
+	plt.figure()
+	plt.subplot(3,1,1) #each of the phis
+	plt.plot(cut["angles"],SEFD(freq, dmax), 'b', label= "SEFD")
+	
+
+
+	plt.title("Radiation Pattern at z = %4.2f" % z)
+	plt.xlabel("Angle [Degrees]")
+	plt.ylabel("Amplitude")
+	# plt.ylim([0, 5E6])
+
+	plt.legend()
+	plt.savefig("../plots/SEFD/" + title)
 
 def Tsys(freq):
 	return 300*(150/freq)**2.5 + 300
@@ -88,7 +138,7 @@ def Tsys(freq):
 def SEFD(freq, dmax):
 	k = 1.38E3 #jy m^2 s k^-1
 	r = 20#m
-	return 2*k*Tsys(freq)*(np.pi*r**2)/calc_app_eff(freq,dmax)
+	return 2*k*Tsys(freq)/(calc_app_eff(freq,dmax)*np.pi*r**2)
 
 
 
--- update_tor.py
+++ update_tor.py
@@ -2,32 +2,58 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import process_grasp
 import subprocess
+import time
+from datetime import datetime
+import pandas as pd
+import os
 
 GRASP_working_file = "F:/Devin/Grasp/LWASandbox/40mLWA/working/"
 model_file = "model_tor.txt"
 out_file = GRASP_working_file + "40mLWA.tor"
 
-for z in [4, 6, 8, 10, 12, 14, 14.5, 15, 15.5, 16]:
+def edit_tor(model, out, z):
+	print("Opening Files")
+	f = open(model,'r')
+	g = open(out,'w+')
 
-	def edit_tor(model, out, z):
-		print("Opening Files")
-		f = open(model,'r')
-		g = open(out,'w+')
+	for i in range(8):
+		g.write(f.readline())
 
+	g.write("  origin           : struct(x: 0.0 m, y: 0.0 m, z: %4.2f m),\n" % z)
+	f.readline()
 
-		for i in range(8):
-			g.write(f.readline())
+	for i in f:
+		g.write(i)
 
-		g.write("  origin           : struct(x: 0.0 m, y: 0.0 m, z: %3.1f m),\n" % z)
-		f.readline()
+	g.close()
+	f.close()
+	print("Done writing TOR")
 
-		for i in f:
-			g.write(i)
+def create_file_w_timestamp(location = os.getcwd()):
+	file_name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+	mydir = os.path.join(
+		location, 
+		file_name)
+	try:
+		os.makedirs(mydir)
+	except OSError as e:
+		if e.errno != errno.EEXIST:
+			raise Exception('File already exists!')
+	return file_name
 
-		g.close()
-		f.close()
-		print("Done writing TOR")
+#########  Generate folders  ###########
+results_path = "../Results/"
+results_path += create_file_w_timestamp(results_path)
+print(results_path)
 
+for direct in ["/plots/", "/plots/Efficiency/", "/plots/SEFD/", "/plots/patterns/"]:
+	if not os.path.exists(results_path + direct):
+		os.mkdir(results_path + direct)
+
+for z in [16]:#np.linspace(11,17.5, 50):
+	direct = results_path + "/plots/patterns/z-%4.2f/"%z
+	if not os.path.exists(direct):
+		os.mkdir(direct)
 
 	edit_tor(model_file, out_file, z)
 
@@ -35,22 +61,25 @@ for z in [4, 6, 8, 10, 12, 14, 14.5, 15, 15.5, 16]:
 	process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, cwd = GRASP_working_file)
 	output, error = process.communicate()
 	print("Done executing grasp, Output:\n")
-	print(output, error)
+	#print(output, error)
 
 
+	#########  Process data files  ###########
 	print("\npreparing to Plot")
 	freq, s11 = process_grasp.process_par(GRASP_working_file + "S_parameters.par")
-
-	dmax = process_grasp.process_cut(GRASP_working_file + "Field_Data.cut", freq)
+	dmax, cut = process_grasp.process_cut(GRASP_working_file + "Field_Data.cut", freq)
 	mis = process_grasp.calc_mismatch(s11)
 	aperture = process_grasp.calc_app_eff(freq, dmax)
 	tsys = process_grasp.Tsys(freq)
 	SEFD = process_grasp.SEFD(freq, dmax)
 	print("Dmax = ", dmax)
-	process_grasp.plot_pair_efficiencies(freq, s11, dmax, "Efficiencies Antenna Position = %3.1f.png" % z, z)
+	process_grasp.plot_pair_efficiencies(freq, s11, dmax, "Efficiencies Antenna Position = %4.2f.png" % z, z)
+	process_grasp.plot_SEFD(freq, dmax, "SEFD Antenna Position = %4.2f.png" % z, z)
 
-	x = open("../data/Results z=%3.1f.csv" %z, 'w+')
-	x.write("frequency [MHz], S11 [dB], Dmax [dBi], Mismatch Efficiency [%], Aperture Efficiency [%], Tsys [K], SEFD [Jy]\n")
+	x = open("../data/Results z=%4.2f.csv" %z, 'w+')
+	x.write(time.strftime("%c"))
+	x.write("\n")
+	x.write("Frequency [MHz], S11 [dB], Dmax [dBi], Mismatch Efficiency, Aperture Efficiency, Tsys [1E3K], SEFD [1E6Jy]\n")
 	for ii in range(len(freq)):
-		x.write("%f, %f, %f, %f, %f, %f, %f\n" % (freq[ii], s11[ii], dmax[ii], mis[ii], aperture[ii], tsys[ii], SEFD[ii]))
+		x.write("%6.2f, %6.2f, %6.2f, %6.2f, %6.2f, %6.2f, %6.2f\n" % (freq[ii], s11[ii], dmax[ii], mis[ii], aperture[ii], tsys[ii]/1E3, SEFD[ii]/1E6))
 	x.close()
