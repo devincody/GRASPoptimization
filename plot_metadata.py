b1462895fcd72e0d9a239f18fbfccceb41c56317
devincody <devincody@gmail.com>
2018-05-11 11:58:37 -0700

    updated plot_metatdata to no index the columns which was causing a OBO bug

--- plot_metadata.py
+++ plot_metadata.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np 
 import os
 import matplotlib.pyplot as plt
+from collections import defaultdict
+
+def conv(x):
+	return float(x.strip())
 
 metadata_path = "../metadata"
 if not os.path.exists(metadata_path):
@@ -12,27 +16,46 @@ cwd = os.getcwd()
 items = os.listdir(cwd)
 for i in items:
 	if ".csv" in i[-5:]:
-		a = pd.read_csv(i, skiprows = 2)
-		loss = ' Loss'
+		print("item: ", i)
+		a = pd.read_csv(i, skiprows = 2, header = 0, index_col=False)
+
+		keys = a.keys()
+		# print(keys)
+		# print(a)
+
+		if "Unnamed" in keys[-1] and " " in a[k[-1]][1]:
+			a = a.drop(keys[-1])
+			print("dropping last column")
+
+		try:
+			loss = ' Loss'
+			a[loss]
+		except:
+			loss = 'Loss'
+			a[loss]
 		a["radius"] = np.sqrt(a['x']**2 + a['z']**2)
 		a["theta"] = np.arctan2(a['z'],a['x'])*180.0/np.pi
+		# print(type(a))
+		# print(type(a[loss]))
+		# print(type(a.loc[:,loss]))
+		# print(type(a.loc[1,loss]))
+		# a[loss] = a[loss].multiply(-1.0)
 		a[loss] *= -1
 
 		path = i[:-4]
 		if not os.path.exists(path):
 			os.mkdir(path)
 
-			loss = ' Loss'
-			parameters = list(a.keys())
-			parameters.remove(loss)
-			for p in parameters:
-				# print p
-				if not "Unnamed:" in p:
-					fig, ax = plt.subplots()
-					ax.scatter(a[p],a[loss], alpha = .8, c = a['alpha']/45, cmap = 'seismic')
-					ax.set_xlabel(p)
-					ax.set_ylabel('Loss')
-					plt.savefig(path + '/' + p + '.png')
-					plt.close()
+		parameters = list(a.keys())
+		parameters.remove(loss)
+		for p in parameters:
+			# print p
+			if not "Unnamed:" in p:
+				fig, ax = plt.subplots()
+				ax.scatter(a[p],a[loss], alpha = .8, c = a['alpha']/45, cmap = 'seismic')
+				ax.set_xlabel(p)
+				ax.set_ylabel('Loss')
+				plt.savefig(path + '/' + p + '.png')
+				plt.close()
 
 
