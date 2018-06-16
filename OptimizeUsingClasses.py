5bd4177e5a340a353778f2720ec106c3482bda99
Devin Cody <devincody@gmail.com>
2018-06-16 14:19:06 -0700

    change to 11Ext

--- OptimizeUsingClasses.py
+++ OptimizeUsingClasses.py
@@ -27,7 +27,7 @@ def main():
 
 	## G1
 	elif platform.node() == 'DESKTOP-3UVMJQF' or platform.node() == 'ASTROS':
-		a = ELfeed(start_f =  60.0, end_f = 85.0, n_f = 20, alpha = 45, grasp_version = 10.3)
+		a = ELfeedExt(start_f =  60.0, end_f = 85.0, n_f = 20, alpha = 45, grasp_version = 10.3)
 		a.set_number_of_focal_lengths(20)
 
 		print("Executing on G1 Office")
